429d3bf2e42cfe87d13983d1f73bd99af4b6a669
Gags50 <125014057+Gags50@users.noreply.github.com>
2024-04-25 14:20:30 +0200

    btuon

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public void showPuzzle(int puzzleID)
+    {
+
+    }
+
+    void hidePuzzle()
+    {
+        
+    }
+
+    void showTip()
+    {
+
+    }
+}
