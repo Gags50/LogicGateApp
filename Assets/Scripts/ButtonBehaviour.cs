09b74d54d8fd2b6fadb8d41148f8c79045a5f6c5
Gags50 <125014057+Gags50@users.noreply.github.com>
2024-04-26 12:55:30 +0200

    butoné funkkk

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -2,18 +2,39 @@ using UnityEngine;
 
 public class ButtonBehaviour : MonoBehaviour
 {
-    public void showPuzzle(int puzzleID)
-    {
+    GameObject canvas;
+    GameObject spawnedPuzzle;
+    GameObject spawnedTip;
+    bool activePuzzle = false;
+    bool activeTip = false;
 
+    private void Awake()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+    public void puzzleToggle(GameObject puzzle)
+    {
+        if (activePuzzle == false)
+        {
+            spawnedPuzzle = Instantiate(puzzle, Vector3.zero, Quaternion.identity, canvas.transform);
+        }
+
+        else
+        {
+            Destroy(spawnedPuzzle);
+        }
     }
 
-    void hidePuzzle()
+    public void tipToggle(GameObject tip)
     {
-        
-    }
+        if (activeTip == false)
+        {
+            spawnedTip = Instantiate(tip, Vector3.zero, Quaternion.identity, canvas.transform);
+        }
 
-    void showTip()
-    {
-
+        else
+        {
+            Destroy(spawnedTip);
+        }
     }
 }
